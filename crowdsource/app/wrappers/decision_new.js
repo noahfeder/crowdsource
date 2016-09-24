babbb3accbee50751a15fab8c70e1c06f0b6d02c
Noah Feder <noah.feder@gmail.com>
2016-09-24 15:57:26 -0400

    attach user id to binaries

--- crowdsource/app/wrappers/decision_new.js
+++ crowdsource/app/wrappers/decision_new.js
@@ -4,46 +4,10 @@ import { Button } from 'react-native-elements';
 import { TouchableHighlight, View, Text } from 'react-native';
 import BackButton from '../components/back_button';
 import { connect } from 'react-redux';
+import Loading from './loading';
 import t from 'tcomb-form-native';
 
-let Form = t.form.Form;
-
-const Binary = t.struct({
-  name: t.String,
-  content: t.String,
-  choiceA: t.String,
-  choiceB: t.String
-});
-
-const formOptions = {
-  auto: 'placeholders',
-  fields: {
-    name: {
-      autoCapitalize: 'words',
-      autoCorrect: true,
-      returnKeyType: 'next',
-      maxLength: 32
-    },
-    content: {
-      multiline: true,
-      autoCapitalize: 'sentences',
-      autoCorrect: true,
-      returnKeyType: 'next'
-    },
-    choiceA: {
-      autoCapitalize: 'words',
-      maxLength: 16,
-      placeholder: 'Choice A'
-    },
-    choiceB: {
-      autoCapitalize: 'words',
-      maxLength: 16,
-      placeholder: 'Choice B'
-    }
-  }
-};
-
-export default class DecisionNew extends Component {
+class DecisionNew extends Component {
 
   onSubmit() {
     let val = this.refs.form.getValue()
@@ -63,20 +27,83 @@ export default class DecisionNew extends Component {
   }
 
   render() {
-    return (
-      <View style={style.wrapper}>
-        <Form
-          ref="form"
-          type={Binary}
-          options={formOptions}
-        />
-        <Button backgroundColor="#2F8"
-          small raised title='SUBMIT'
-          onPress={this.onSubmit.bind(this)}
-        />
-        <BackButton navigator={this.props.navigator} />
-      </View>
-    )
+    if (this.props.loaded) {
+      let Form = t.form.Form;
+
+      const Binary = t.struct({
+        name: t.String,
+        content: t.String,
+        choiceA: t.String,
+        choiceB: t.String,
+        id: t.String
+      });
+
+      const Value = {id: this.props.id}
+
+      const formOptions = {
+        auto: 'placeholders',
+        fields: {
+          id: {
+            hidden: true
+          },
+          name: {
+            autoCapitalize: 'words',
+            autoCorrect: true,
+            returnKeyType: 'next',
+            maxLength: 32
+          },
+          content: {
+            multiline: true,
+            autoCapitalize: 'sentences',
+            autoCorrect: true,
+            returnKeyType: 'next'
+          },
+          choiceA: {
+            autoCapitalize: 'words',
+            maxLength: 16,
+            placeholder: 'Choice A'
+          },
+          choiceB: {
+            autoCapitalize: 'words',
+            maxLength: 16,
+            placeholder: 'Choice B'
+          }
+        }
+      };
+
+      return (
+        <View style={style.wrapper}>
+          <Form
+            ref="form"
+            type={Binary}
+            value={Value}
+            options={formOptions}
+          />
+          <Button backgroundColor="#2F8"
+            small raised title='SUBMIT'
+            onPress={this.onSubmit.bind(this)}
+          />
+          <BackButton navigator={this.props.navigator} />
+        </View>
+      )
+    } else {
+      return <Loading />
+    }
+
   }
 }
 
+function mapStateToProps(state) {
+  if (state.user) {
+    return {
+      loaded: true,
+      id: state.user.id
+    };
+  } else {
+    return {
+      loaded: false
+    };
+  }
+}
+
+export default connect(mapStateToProps)(DecisionNew);
