9cdee60b32701d54005d82df3fd4699da4b477b6
Noah Feder <noah.feder@gmail.com>
2016-09-24 00:58:46 -0400

    move server to other repo

--- cs-server/db/migrate/20160922041449_add_cols_to_binaries.rb
+++
@@ -1,6 +0,0 @@
-class AddColsToBinaries < ActiveRecord::Migration[5.0]
-  def change
-    add_column :binaries, :choiceA, :string
-    add_column :binaries, :choiceB, :string
-  end
-end
--- cs-server/db/migrate/20160922232912_add_expiration_to_binaries.rb
+++
@@ -1,5 +0,0 @@
-class AddExpirationToBinaries < ActiveRecord::Migration[5.0]
-  def change
-    add_column :binaries, :expiration, :string
-  end
-end
