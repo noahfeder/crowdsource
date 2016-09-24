9eedd94d69a4dbed77da2ada71760e265565abe2
Noah Feder <noah.feder@gmail.com>
2016-09-24 16:53:38 -0400

    style new binary form

--- crowdsource/app/wrappers/decision_new.js
+++ crowdsource/app/wrappers/decision_new.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import style from '../public/styles/style';
 import { Button } from 'react-native-elements';
-import { TouchableHighlight, View, Text } from 'react-native';
+import { TouchableHighlight, View, Text, Dimensions } from 'react-native';
 import BackButton from '../components/back_button';
 import { connect } from 'react-redux';
+import Header from '../components/header';
 import Loading from './loading';
 import t from 'tcomb-form-native';
+import _ from 'lodash';
 
 class DecisionNew extends Component {
 
@@ -30,17 +32,59 @@ class DecisionNew extends Component {
     if (this.props.loaded) {
       let Form = t.form.Form;
 
+      const TimeType = t.enums({
+        day: 'Days',
+        hours: 'Hours',
+        minutes: 'Minutes'
+      });
+
       const Binary = t.struct({
         name: t.String,
         content: t.String,
         choiceA: t.String,
         choiceB: t.String,
+        number: t.Number,
+        type: TimeType,
         id: t.String
       });
 
-      const Value = {id: this.props.id}
+      const Value = {id: this.props.id, type: 'hours'}
+
+      let { height, width } = Dimensions.get('window');
+
+      const halfStyle = _.cloneDeep(t.form.Form.stylesheet);
+      const newStyle = _.cloneDeep(t.form.Form.stylesheet);
+      const tallStyle = _.cloneDeep(t.form.Form.stylesheet);
+
+      tallStyle.textbox.normal.height = 100;
+      tallStyle.textbox.error.height = 100;
+      tallStyle.textbox.normal.width = width - 20;
+      tallStyle.textbox.error.width = width - 20;
+      tallStyle.textbox.normal.margin = 10
+      tallStyle.textbox.error.margin = 10
+
+      newStyle.fieldset = {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        justifyContent: 'space-around'
+      }
+      newStyle.textbox.normal.width = width - 20;
+      newStyle.textbox.error.width = width - 20;
+      newStyle.textbox.normal.margin = 10
+      newStyle.textbox.error.margin = 10
+
+      halfStyle.textbox.normal.width = width / 2 - 20;
+      halfStyle.textbox.error.width = width / 2 - 20;
+      halfStyle.pickerTouchable.normal.width = width / 2 - 20;
+      halfStyle.pickerTouchable.error.width = width / 2 - 20;
+
+
+
+      console.log(halfStyle)
 
       const formOptions = {
+        stylesheet: newStyle,
         auto: 'placeholders',
         fields: {
           id: {
@@ -56,23 +100,33 @@ class DecisionNew extends Component {
             multiline: true,
             autoCapitalize: 'sentences',
             autoCorrect: true,
-            returnKeyType: 'next'
+            returnKeyType: 'next',
+            stylesheet: tallStyle
           },
           choiceA: {
             autoCapitalize: 'words',
             maxLength: 16,
-            placeholder: 'Choice A'
+            placeholder: 'Choice A',
+            stylesheet: halfStyle
           },
           choiceB: {
             autoCapitalize: 'words',
             maxLength: 16,
-            placeholder: 'Choice B'
+            placeholder: 'Choice B',
+            stylesheet: halfStyle
+          },
+          type: {
+            stylesheet: halfStyle
+          },
+          number: {
+            stylesheet: halfStyle
           }
         }
       };
 
       return (
         <View style={style.wrapper}>
+          <Header />
           <Form
             ref="form"
             type={Binary}
